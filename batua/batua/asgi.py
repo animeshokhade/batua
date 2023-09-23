12652cae602d09df343fb9670647d8573926e82b
Animesh Okhade <animesh.okhade.private@gmail.com>
2023-09-23 22:07:37 +0530

    Hello World

---
+++ batua/batua/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for batua project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'batua.settings')
+
+application = get_asgi_application()
